a8eb5aa3fbe78d6d3288d256074e5e2796227a7e
Fan <victorlifan@vip.qq.com>
2021-10-04 13:21:38 -0700

    Update corss_join.sql
    add conclusion col

--- corss_join.sql
+++ corss_join.sql
@@ -2,8 +2,8 @@
 DROP TABLE IF EXISTS side_a;
 DROP TABLE IF EXISTS side_b;
 -- create tables
-CREATE TABLE side_a (A INT NOT NULL);
-CREATE TABLE side_b (B INT NOT NULL);
+CREATE TABLE side_a (side_a INT NOT NULL);
+CREATE TABLE side_b (side_b INT NOT NULL);
 -- insert records
 INSERT INTO side_a VALUES (20);
 INSERT INTO side_a VALUES (20);
@@ -22,8 +22,13 @@ SELECT * FROM side_b;
 
 -- CROSS JOIN 
 -- Assume side_c is 40 so that the only condition needed to be satisfied in order to form a triangle is for a+b>c
-SELECT a,b, a+b AS combination
-FROM side_a
-CROSS JOIN
-side_b
-ORDER BY 3 DESC;
+SELECT *,
+	CASE WHEN combination> 40 THEN 'Triangle'
+		 ELSE 'Not Triangle'
+		 END AS conclusion
+FROM (
+	SELECT side_a, side_b, side_a+side_b AS combination
+	FROM side_a
+	CROSS JOIN
+	side_b
+	ORDER BY 3 DESC ) a;
